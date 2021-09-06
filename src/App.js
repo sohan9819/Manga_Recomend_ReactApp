d9e94e39176372d1941ebe9bab9aaf0020380a86
SnickerDev <64985447+sohan9819@users.noreply.github.com>
2021-09-06 23:18:40 +0530

    Edited ratings and season numbers

--- src/App.js
+++ src/App.js
@@ -3,53 +3,55 @@ import React, { useState } from "react";
 
 const mangaDict = {
   Adventure: [
-    "One Piece",
-    "Fairy Tail",
-    "Hunter X Hunter",
-    "Kino's Journey",
-    "Spice and Wolf"
+    { "One Piece": { rating: 9.5, numberOfSeasons: 4 } },
+    { "Fairy Tail": { rating: 8, numberOfSeasons: 1 } },
+    { "Hunter X Hunter": { rating: 8.5, numberOfSeasons: 6 } },
+    { "Kino's Journey": { rating: 8, numberOfSeasons: 2 } },
+    { "Spice and Wolf": { rating: 9, numberOfSeasons: 4 } }
   ],
   Comedy: [
-    "Gintama",
-    "The Melancholy of Haruhi Suzumiya",
-    "The Devil Is a Part-Timer!",
-    "Daily Lives of High School Boys",
-    "Nichijou"
+    { Gintama: { rating: 9, numberOfSeasons: 4 } },
+    { "The Melancholy of Haruhi Suzumiya": { rating: 9, numberOfSeasons: 4 } },
+    { "The Devil Is a Part-Timer!": { rating: 9, numberOfSeasons: 4 } },
+    { "Daily Lives of High School Boys": { rating: 9, numberOfSeasons: 4 } },
+    { Nichijou: { rating: 9, numberOfSeasons: 4 } }
   ],
   Drama: [
-    "Clannad",
-    "Anohana: The Flower We Saw That Day",
-    "Your Lie in April",
-    "Kanon",
-    "Little Busters!"
+    { Clannad: { rating: 9.3, numberOfSeasons: 2 } },
+    {
+      "Anohana: The Flower We Saw That Day": { rating: 9, numberOfSeasons: 4 }
+    },
+    { "Your Lie in April": { rating: 9, numberOfSeasons: 1 } },
+    { Kanon: { rating: 8.7, numberOfSeasons: 1 } },
+    { "Little Busters!": { rating: 8.5, numberOfSeasons: 2 } }
   ],
   Slice_of_Life: [
-    "Barakamon",
-    "Non Non Biyori",
-    "Koufuku Graffiti",
-    "Lucky Star",
-    "Wagnaria!!"
+    { Barakamon: { rating: 7, numberOfSeasons: 14 } },
+    { "Non Non Biyori": { rating: 6, numberOfSeasons: 11 } },
+    { "Koufuku Graffiti": { rating: 8, numberOfSeasons: 7 } },
+    { "Lucky Star": { rating: 9, numberOfSeasons: 6 } },
+    { "Wagnaria!!": { rating: 9.5, numberOfSeasons: "41+" } }
   ],
   Fantasy: [
-    "No Game No Life",
-    "Nanatsu no Taizai",
-    "Inuyasha",
-    "Escaflowne",
-    "Slayers"
+    { "No Game No Life": { rating: 9.5, numberOfSeasons: 14 } },
+    { "Nanatsu no Taizai": { rating: 8, numberOfSeasons: 32 } },
+    { Inuyasha: { rating: 7, numberOfSeasons: 2 } },
+    { Escaflowne: { rating: 7.5, numberOfSeasons: 3 } },
+    { Slayers: { rating: 9, numberOfSeasons: 1 } }
   ],
   Horror: [
-    "Junji Ito Collection",
-    "Parasyte -the maxim-",
-    "Another",
-    "School-Live!",
-    "Hell Girl"
+    { "Junji Ito Collection": { rating: 8, numberOfSeasons: 14 } },
+    { "Parasyte -the maxim-": { rating: 9.5, numberOfSeasons: 22 } },
+    { Another: { rating: 9, numberOfSeasons: 3 } },
+    { "School-Live!": { rating: 8, numberOfSeasons: 4 } },
+    { "Hell Girl": { rating: 9.5, numberOfSeasons: 2 } }
   ],
   Psychological: [
-    "Death Note",
-    "Mirai Nikki",
-    "Zetsuen no Tempest",
-    "Monster",
-    "Paranoia Agent"
+    { "Death Note": { rating: 10, numberOfSeasons: 1 } },
+    { "Mirai Nikki": { rating: 9, numberOfSeasons: 4 } },
+    { "Zetsuen no Tempest": { rating: 8.5, numberOfSeasons: 6 } },
+    { Monster: { rating: 9, numberOfSeasons: 2 } },
+    { "Paranoia Agent": { rating: 8, numberOfSeasons: 10 } }
   ]
 };
 
@@ -86,9 +88,18 @@ export default function App() {
         })}
       </ul>
       <h2 id="genreHeding">{genre}</h2>
-      <ul>
+      <ul id="mangaList">
         {selectedGenre.map((item) => {
-          return <li id="manga"> {item} </li>;
+          const mainName = Object.keys(item);
+          console.log(mainName);
+          console.log(item[mainName]["rating"]);
+          return (
+            <li id="manga">
+              {mainName}
+              <h5>Rating : {item[mainName]["rating"]} / 10</h5>
+              <h5>Number_Of_Seasons : {item[mainName]["numberOfSeasons"]}</h5>
+            </li>
+          );
         })}
       </ul>
     </div>
